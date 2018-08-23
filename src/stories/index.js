43489ce5b5f0eed8e7e7ea6abb1fa647f26f0c7d
Michal Muszynski <m.muszy@gmail.com>
2018-08-23 19:23:54 +0200

    Story of DetailedContactInfo

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import NewContactPanel from '../components/NewContactPanel/NewContactPanel'
+import ContactDetailedInfo from '../components/ContactDetailedInfo/ContactDetailedInfo'
 
 storiesOf('NewContactPanel', module)
   .add('Default view', () => (
     <NewContactPanel />
   ));
 
+storiesOf('ContactDetailedInfo', module)
+  .add('Default view', () => (
+    <ContactDetailedInfo />
+  ));
 
 
 
