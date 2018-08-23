921c755d9640f602c3042eee1181ec84198559b7
Michal Muszynski <m.muszy@gmail.com>
2018-08-23 19:25:12 +0200

    Story of EditFormInputs

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import NewContactPanel from '../components/NewContactPanel/NewContactPanel'
 import ContactDetailedInfo from '../components/ContactDetailedInfo/ContactDetailedInfo'
+import EditFormInputs from '../components/EditFormInputs/EditFormInputs'
 
 storiesOf('NewContactPanel', module)
   .add('Default view', () => (
@@ -11,6 +12,11 @@ storiesOf('NewContactPanel', module)
 storiesOf('ContactDetailedInfo', module)
   .add('Default view', () => (
     <ContactDetailedInfo />
+  ));
+
+storiesOf('EditFormInputs', module)
+  .add('Default view', () => (
+    <EditFormInputs />
   ));
 
 
