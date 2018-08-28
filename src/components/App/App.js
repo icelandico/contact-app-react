ae084ce13c153a8005515644031afe9c3e868321
Michal Muszynski <m.muszy@gmail.com>
2018-08-28 19:09:27 +0200

    Create Header and Main components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,51 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import NewContactPanel from '../NewContactPanel/NewContactPanel'
-import ContactList from '../ContactsList/ContactsList'
+import Main from '../Main/Main'
+import Header from '../Header/Header'
+
 
 class App extends Component {
-
-  state = {
-    contacts: [],
-  };
-
-  getContacts = () => {
-    fetch(`http://localhost:3000/contacts`)
-      .then(results => {
-        return results.json()
-      })
-      .then(data => {
-        const contactList = data.sort(this.sortContacts).map(contact => contact);
-        this.setState({
-          contacts: contactList
-        });
-        localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-      });
-  };
-
-  sortContacts = (contactA, contactB) => {
-    if (contactA.lastName.toLowerCase() < contactB.lastName.toLowerCase()) {
-      return -1;
-    } else if (contactA.lastName.toLowerCase() > contactB.lastName.toLowerCase()) {
-      return 1;
-    }
-    return 0;
-  };
-
-  componentDidMount() {
-    this.getContacts();
-  }
 
   render() {
 
     return (
       <div className="App">
         <h1>Contact App</h1>
-        <NewContactPanel getContacts={this.getContacts}/>
-        <ContactList  retrievedContacts={this.state.contacts}
-                      sortContacts={this.sortContacts}
-                      getContacts={this.getContacts}
-        />
+        <Header />
+        <Main />
       </div>
     );
   }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import NewContactPanel from '../NewContactPanel/NewContactPanel'
+import ContactList from '../ContactsList/ContactsList'
+
+class Main extends Component {
+
+  state = {
+    contacts: [],
+  };
+
+  getContacts = () => {
+    fetch(`http://localhost:3000/contacts`)
+      .then(results => {
+        return results.json()
+      })
+      .then(data => {
+        const contactList = data.sort(this.sortContacts).map(contact => contact);
+        this.setState({
+          contacts: contactList
+        });
+        localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+      });
+  };
+
+  sortContacts = (contactA, contactB) => {
+    if (contactA.lastName.toLowerCase() < contactB.lastName.toLowerCase()) {
+      return -1;
+    } else if (contactA.lastName.toLowerCase() > contactB.lastName.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  };
+
+  componentDidMount() {
+    this.getContacts();
+  }
+
+  render() {
+    return (
+      <div>
+        <NewContactPanel getContacts={this.getContacts}/>
+        <ContactList  retrievedContacts={this.state.contacts}
+                      sortContacts={this.sortContacts}
+                      getContacts={this.getContacts}
+        />
+      </div>
+    )
+  }
+}
+
+export default Main
+
