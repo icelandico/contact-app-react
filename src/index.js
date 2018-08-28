80afe99f1418017f6b273a1d87c2570a2f1609fa
Michal Muszynski <m.muszy@gmail.com>
2018-08-28 20:01:12 +0200

    Refactored

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App'
 import './index.css';
-import { Router } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>, document.getElementById('root'));
+  </HashRouter>, document.getElementById('root'));
 registerServiceWorker();
