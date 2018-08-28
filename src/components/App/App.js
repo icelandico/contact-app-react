8cd5513a42e9a6c2fd30ada5ff6e82661cef50fc
Michal Muszynski <m.muszy@gmail.com>
2018-08-28 20:21:17 +0200

    Refactored

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-
 import Main from '../Main/Main'
 import Header from '../Header/Header'
 
 class App extends Component {
 
   render() {
-
     return (
       <div>
         <h1>Choose Your destiny</h1>
